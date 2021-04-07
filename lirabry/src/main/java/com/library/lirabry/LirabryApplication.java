658210e82d509179aa8c4898625cdadda4c53544
yovo <yovo.manolov@abv.bg>
2021-04-07 23:31:09 +0300

    Project initialization, first commit
    07.04.2021

---
+++ lirabry/src/main/java/com/library/lirabry/LirabryApplication.java
@@ -0,0 +1,13 @@
+package com.library.lirabry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LirabryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LirabryApplication.class, args);
+	}
+
+}
